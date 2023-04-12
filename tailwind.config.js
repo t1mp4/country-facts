316fe3690a5be0e849ec4aaa5a1e8705c2e254c0
t1mp4 <timothytampold@gmail.com>
2023-04-12 19:10:10 +0200

    Set up some base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    fontFamily: {
+      sans: 'var(--ff-nunito-sans)', // Defined in root layout
+    },
+    fontWeight: {
+      light: 300,
+      normal: 400,
+      semibold: 600,
+      extrabold: 800,
+    },
+    colors: {
+      text: {
+        primary: 'var(--clr-text-primary)',
+      },
+      bg: {
+        primary: 'var(--clr-bg-primary)',
+        secondary: 'var(--clr-bg-secondary)',
       },
     },
+    extend: {},
   },
   plugins: [],
-}
+};
