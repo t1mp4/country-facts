f06995297444f1669942be2c569fcdffe1309d3c
t1mp4 <timothytampold@gmail.com>
2023-04-12 19:10:24 +0200

    Set up font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
+import { Nunito_Sans } from 'next/font/google';
+import Link from 'next/link';
 import './globals.css';
+
+const nunitoSans = Nunito_Sans({
+  weight: ['300', '400', '600', '800'],
+  subsets: ['latin'],
+  variable: '--ff-nunito-sans',
+});
 
 export const metadata = {
   title: 'Create Next App',
@@ -7,8 +15,38 @@ export const metadata = {
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={nunitoSans.variable}>
+      <body>
+        <header>
+          <Link href="/" aria-label="Home">
+            Where in the world?
+          </Link>
+
+          <label>
+            <input
+              type="checkbox"
+              id="dark-mode-input"
+              className="sr-only"
+              aria-label="Dark mode activated"
+            />
+            <span aria-hidden>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+              >
+                <path
+                  d="M10 7C10 10.866 13.134 14 17 14C18.9584 14 20.729 13.1957 21.9995 11.8995C22 11.933 22 11.9665 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C12.0335 2 12.067 2 12.1005 2.00049C10.8043 3.27098 10 5.04157 10 7ZM4 12C4 16.4183 7.58172 20 12 20C15.0583 20 17.7158 18.2839 19.062 15.7621C18.3945 15.9187 17.7035 16 17 16C12.0294 16 8 11.9706 8 7C8 6.29648 8.08133 5.60547 8.2379 4.938C5.71611 6.28423 4 8.9417 4 12Z"
+                  fill="rgba(246,8,8,1)"
+                />
+              </svg>
+              Dark mode
+            </span>
+          </label>
+        </header>
+        {children}
+      </body>
     </html>
   );
 };
